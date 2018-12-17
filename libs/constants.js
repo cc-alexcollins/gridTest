efdf3b9a3da5ccf975c414cf2c7c29b0f3670ab3
Alex Collins <alex@cloudcade.com>
2018-12-17 15:17:53 -0800

    Add Documentation To Constants

--- libs/constants.js
+++ libs/constants.js
@@ -1,16 +1,85 @@
+/**
+ * @module Constants
+ *
+ * @description Generation constant values
+ */
+
 const Helper = require('./helper');
 
+/**
+ * @constant randSeed
+ *
+ * @description The seed used for all random number generation
+ * @see [Random](#module_Random)
+ *
+ * @returns {int} The seed
+ */
 exports.randSeed = 8;
 
+/**
+ * @constant minNodeGroups
+ *
+ * @description The minimum number of groups a given level can have
+ *
+ * @returns {int} The number of groups
+ */
 exports.minNodeGroups = 1;
+
+/**
+ * @constant maxNodeGroups
+ *
+ * @description The maximum number of groups a given level can have
+ *
+ * @returns {int} The number of groups
+ */
 exports.maxNodeGroups = 3;
 
+/**
+ * @constant minNodesPerGroup
+ *
+ * @description The minimum number of nodes a given group can have
+ *
+ * @returns {int} The number of nodes in a group
+ */
 exports.minNodesPerGroup = 1;
+
+/**
+ * @constant maxNodesPerGroup
+ *
+ * @description The maximum number of nodes a given group can have
+ *
+ * @returns {int} The number of nodes in a group
+ */
 exports.maxNodesPerGroup = 4;
 
+/**
+ * @constant maxNodesPerLevel
+ *
+ * @description
+ * The maximum number of nodes a single level can have across all groups
+ *
+ * @returns {int} The number of nodes for a level
+ */
 exports.maxNodesPerLevel = 6;
 
+/**
+ * @constant minStartingNodes
+ *
+ * @description The minimum number of nodes to start the grid with. Only applies
+ * to the first level.
+ *
+ * @returns {int} The number of nodes
+ */
 exports.minStartingNodes = 3;
+
+/**
+ * @constant maxStartingNodes
+ *
+ * @description The maximum number of nodes to start the grid with. Only applies
+ * to the first level.
+ *
+ * @returns {int} The number of nodes
+ */
 exports.maxStartingNodes = 5;
 
 const allowedGroups = [
@@ -29,13 +98,29 @@ const allowedGroups = [
   [1, 1, 1]
 ];
 
-// Get variations (i.e. [2, 3])
 const foundVariations = [];
+
+/**
+ * @constant allowedGroupVariations
+ *
+ * @description All of the legal group variation permutations
+ *
+ * @returns {Array.<int[]>}
+ * The group variations, with each variation being an array of integers indexed
+ * by group, and the integer being the count in that group
+ */
 exports.allowedGroupVariations = allowedGroups.reduce((allGroups, group) => {
   addGroups(allGroups, group, []);
   return allGroups;
 }, []);
 
+/**
+ * @constant elements
+ *
+ * @description The name of the node within a given level. Used for printing.
+ *
+ * @returns {string[]} The name of the node for printing
+ */
 exports.elements = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 
 function addGroups(allGroups, group, start) {
